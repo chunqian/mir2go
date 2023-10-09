11a245ca505e521d2be380003809740f5f5a47d0
chunqian.shen <chunqian.shen@qq.com>
2023-10-09 10:58:30 +0800

    Add

---
+++ gate/select/0.go
@@ -0,0 +1,7 @@
+//go:build darwin
+
+// Copyright (C) 2023 CHUNQIAN SHEN. All rights reserved.
+
+package main
+
+import _ "github.com/chunqian/mir2go/pkgs/macapp"
---
+++ gate/select/main.go
@@ -0,0 +1,16 @@
+// Copyright (C) 2023 CHUNQIAN SHEN. All rights reserved.
+
+package main
+
+import (
+    "github.com/ying32/govcl/vcl"
+)
+
+var (
+    AppDir string = "/Users/tim/Downloads/github/mir2go/bin/selgate/"
+)
+
+func main() {
+    vcl.DEBUG = true
+    vcl.RunApp()
+}
